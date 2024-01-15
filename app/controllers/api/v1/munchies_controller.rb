e76ada512cf7308d923563fac1d16c68ef544237
Scott DeVoss <scottdevoss@yahoo.com>
2024-01-15 09:00:52 -0700

    made weather api connection

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
-
+    conn = Faraday.new(url: "http://api.weatherapi.com") do |faraday|
+      faraday.headers["weather"] = Rails.application.credentials.weather[:key]
+    end
+    response = conn.get("/v1/forecast.json?key=#{Rails.application.credentials.weather[:key]}&q=pueblo,co&days=5")
+    
+    data = JSON.parse(response.body, symbolize_names: true)
+    require 'pry'; binding.pry
   end
 end
--- spec/requests/api/v0/munchies_spec.rb
+++ spec/requests/api/v0/munchies_spec.rb
@@ -12,3 +12,23 @@ RSpec.describe "Api::V0::Munchies", type: :request do
     end 
   end
 end
+
+# {
+#   "data": {
+#     "id": "null",
+#     "type": "munchie",
+#     "attributes": {
+#       "destination_city": "Pueblo, CO",
+#       "forecast": {
+#         "summary": "Cloudy with a chance of meatballs",
+#         "temperature": "83"
+#       },
+#       "restaurant": {
+#         "name": "La Forchetta Da Massi",
+#         "address": "126 S Union Ave, Pueblo, CO 81003",
+#         "rating": 4.5,
+#         "reviews": 148
+#       }
+#     }
+#   }
+# }
