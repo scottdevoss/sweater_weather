d46746e0486fc2ab3148cdc04578df5add9d73a9
Scott DeVoss <scottdevoss@yahoo.com>
2024-01-15 10:59:17 -0700

    added munchie serializer

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -6,17 +6,16 @@ class Api::V1::MunchiesController < ApplicationController
     response = conn.get("/v1/forecast.json?key=#{Rails.application.credentials.weather[:key]}&q=pueblo,co&days=5")
     
     data = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
+    
   end
 
   def reviews
     conn = Faraday.new(url: "https://api.yelp.com") do |faraday|
-      require 'pry'; binding.pry
       faraday.headers["yelp"] = Rails.application.credentials.yelp[:key]
     end
     response = conn.get("/v3/businesses/north-india-restaurant-san-francisco/reviews")
     
     data = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
+  
   end
 end
