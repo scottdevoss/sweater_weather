99299f091b471f82cd9b67bf6d6a6ca5a97441c2
Scott DeVoss <scottdevoss@yahoo.com>
2024-01-16 11:32:52 -0700

    did not finish

--- app/controllers/api/v1/book_controller.rb
+++ app/controllers/api/v1/book_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::BookController < ApplicationController
   def search
-    
+    location = params[:location]
+
+    conn = Faraday.new(url: "https://openlibrary.org") do |faraday|
+
+    end
+
+    response = conn.get("/search.json")
+
+    data = JSON.parse(response.body, symbolize_keys: true)
+      
   end
 end
