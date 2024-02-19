30d6a1d233f11b7991cc2141e88412d4e86dcc82
Scott DeVoss <scottdevoss@yahoo.com>
2024-02-19 12:01:33 -0700

    completed first user story

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -1,17 +1,7 @@
 class Api::V0::ForecastController < ApplicationController
   def index
     location = LocationFacade.new(params[:location]).location.lat_and_long
-    weather = WeatherFacade.new.location_weather(location)
-
-    # conn = Faraday.new(url: "http://api.weatherapi.com") do |faraday|
-    #   faraday.params[:key] = Rails.application.credentials.weather[:key]
-    # end
-
-    # response = conn.get("/v1/forecast.json?q=#{location}&days=5")
-
-    # data = JSON.parse(response.body, symbolize_names: true)
-    # Weather.new(data)
-    
-    # require 'pry'; binding.pry
+    weather = WeatherSerializer.new(WeatherFacade.new.location_weather(location))
+    render json: weather
   end
 end
--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -1,10 +1,49 @@
 class Weather
-  attr_reader :data
+  attr_reader :id, :data
   def initialize(data)
     @data = data
+    @id = nil
   end
 
-  # def current_weather
-  #   require 'pry'; binding.pry
-  # end
+  def current_weather
+    current = {}
+    current[:last_updated] = @data[:current][:last_updated]
+    current[:temperature] = @data[:current][:temp_f]
+    current[:feels_like] = @data[:current][:feelslike_f]
+    current[:humidity] = @data[:current][:humidity]
+    current[:uvi] = @data[:current][:uv]
+    current[:visibility] = @data[:current][:vis_miles]
+    current[:condition] = @data[:current][:condition][:text]
+    current[:icon] = @data[:current][:condition][:icon]
+    current
+  end
+
+  def daily_weather
+    daily = []
+    @data[:forecast][:forecastday].each do |date|
+      daily << {
+        date: date[:date],
+        sunrise: date[:astro][:sunrise],
+        sunset: date[:astro][:sunset],
+        max_temp: date[:day][:maxtemp_f],
+        min_temp: date[:day][:mintemp_f],
+        condition: date[:day][:condition][:text],
+        icon: date[:day][:condition][:icon],
+      }
+    end
+    daily
+  end
+
+  def hourly_weather
+    hourly = []
+    @data[:forecast][:forecastday][0][:hour].each do |each_hr|
+      hourly << {
+        time: DateTime.parse(each_hr[:time]).strftime("%H:%M"),
+        temperature: each_hr[:temp_f],
+        conditions: each_hr[:condition][:text],
+        icon: each_hr[:condition][:icon]
+      }
+    end
+    hourly
+  end
 end
